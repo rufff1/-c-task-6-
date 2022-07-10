ad73fedfdc8f7dd93433538ab7a8defa8cb4ac24
rufff1 <rufatzg@code.edu.az>
2022-07-11 01:38:43 +0400

    c# task 6

---
+++ task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace task1
+{
+    public class task1
+    {
+        static void Main()
+        {
+            DocumentProgram basicprogram = new DocumentProgram();
+            ExpertDocumentProgram expertDocumentProgram = new ExpertDocumentProgram();
+            ProDocumentProgram proDocumentProgram = new ProDocumentProgram();
+
+
+            basicprogram.OpenDocument();
+            basicprogram.EditDocument();
+            basicprogram.SaveDocument();
+
+            //expertDocumentProgram.OpenDocument();
+            //expertDocumentProgram.EditDocument();
+            //expertDocumentProgram.SaveDocument();
+
+
+            //proDocumentProgram.OpenDocument();
+            //proDocumentProgram.EditDocument();
+            //proDocumentProgram.SaveDocument();
+
+        }
+    }
+}
